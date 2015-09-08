ea87708ee9b8ace4047b0a177477df50e96a09a6
Otto van Verseveld <ohvanverseveld@gmail.com>
2015-09-08 14:07:55 +0200

    Added new cout ...

--- QtGitHello/main.cpp
+++ QtGitHello/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     cout << "Hello Git!" << endl;
+    cout << "Successfully cloned from GitHub using SourceTree." << endl;
 
     return a.exec();
 }
